3d63bcdb26cc267e56814eae0080f7a0785c716e
Aditya Singh Tomar <adityasinghtomar52@gmail.com>
2026-09-01 15:03:39 +0530

    Prisma

---
+++ Backend/user_service/src/config/index.js
@@ -0,0 +1,14 @@
+const config = {
+    SERVICE_NAME: require('../../package.json').name,
+    PORT: Number(process.env.PORT) || 4002,
+    NODE_ENV: process.env.NODE_ENV || "development",
+    LOG_LEVEL: process.env.LOG_LEVEL || "info",
+    ELASTICSEARCH_URL: process.env.ELASTICSEARCH_URL,
+    KAFKA_BROKER: process.env.KAFKA_BROKER,
+    KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID,
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
+}
+
+module.exports = { config };
+
+//with the help of this instead of wrting process.env. this we will write config.port 
---
+++ Backend/user_service/src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const helmet = require('helmet');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { config } = require("dotenv");
+const logger = require("./config/logger");
+
+const { corsMiddleware } = require("./middleware/cors.middleware");
+const errorHandler = require('./middlewares/error.middleware');
+const { reqLogger } = require("./middlewares/req.middleware");
+
+const app = express();
+
+
+app.use(helmet());
+app.use(corsMiddleware());
+app.use(cookieParser());
+app.use(reqLogger);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello  from index.js of user-service")
+
+
+})
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "User service is healthy" });
+});
+
+app.use(errorHandler)
+
+const startServer = () => {
+    try {
+        const server = app.listen(config.PORT, () => {
+            logger.info(
+                `${config.SERVICE_NAME} is running on port ${config.PORT}`
+
+            );
+        })
+
+    } catch (error) {
+        logger.error(`Error starting ${config.SERVICE_NAME}: ${error.message}`);
+
+    }
+}
+startServer();
