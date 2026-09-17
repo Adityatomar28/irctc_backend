807a19411bae51a686fddbe16a8401ceed6c9660
Aditya Singh Tomar <adityasinghtomar52@gmail.com>
2026-09-17 14:28:22 +0530

    Admin service ,api gateway

---
+++ Backend/admin_service/src/routes/auth.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { sendOTP, verifyOTP, login, rotateRefreshToken, verifyGoogleIdToken } = require('../controllers/auth.controller');
+
+router.post("/send-otp", sendOTP);
+router.post("/verify-otp", verifyOTP);
+router.post("/login", login);
+
+module.exports = router;
---
+++ Backend/api_gateway/src/services/proxy.js
@@ -0,0 +1,7 @@
+//Proxy 
+/**
+ * It manages 3 things 
+ * 1)circuit breaker
+ * 2)It forward request to the downstream service
+ * 3)it implement function of proxy
+ */
